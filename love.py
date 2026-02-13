95d0b376f3fc0f52d80b6adeca65d9cf3a8ca992
Arijit8927 <abhijitpal4523@gmail.com>
2026-02-13 12:49:59 +0530

    Add files via upload

---
+++ love.py
@@ -0,0 +1,35 @@
+from flask import Flask,  redirect, url_for,render_template
+import os
+
+
+love = Flask(__name__)
+love.secret_key = os.environ.get('SECRET_KEY', 'your-love-secret-key-2026')
+@love.route('/')
+def home():
+    return render_template("lamp.html")
+
+@love.route('/user')
+def user(): 
+    # return redirect(url_for("Love.html"))
+    return render_template("Love.html")
+@love.route('/love2')
+def love2(): 
+    # return redirect(url_for("Love.html"))
+    return render_template("love2.html")
+@love.route('/chocolate')
+def chocolate2():
+    return render_template("chocolate2.html")
+@love.route('/love3')
+def love3(): 
+    # return redirect(url_for("Love.html"))
+    return render_template("love3.html")
+@love.route('/love4')
+def love4(): 
+    # return redirect(url_for("Love.html"))
+    return render_template("love4.html")
+@love.route('/pic')
+def pic():
+    return render_template("pic.html")
+
+if __name__ == '__main__':
+    love.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
