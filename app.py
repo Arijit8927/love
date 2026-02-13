5b2fe7e5c01b026a585301aded2b40bbb410e672
Arijit8927 <abhijitpal4523@gmail.com>
2026-02-13 12:56:43 +0530

    app

--- app.py
+++ app.py
@@ -2,34 +2,34 @@ from flask import Flask,  redirect, url_for,render_template
 import os
 
 
-love = Flask(__name__)
+app = Flask(__name__)
 love.secret_key = os.environ.get('SECRET_KEY', 'your-love-secret-key-2026')
-@love.route('/')
+@app.route('/')
 def home():
     return render_template("lamp.html")
 
-@love.route('/user')
+@app.route('/user')
 def user(): 
     # return redirect(url_for("Love.html"))
     return render_template("Love.html")
-@love.route('/love2')
+@app.route('/love2')
 def love2(): 
     # return redirect(url_for("Love.html"))
     return render_template("love2.html")
-@love.route('/chocolate')
+@app.route('/chocolate')
 def chocolate2():
     return render_template("chocolate2.html")
-@love.route('/love3')
+@app.route('/love3')
 def love3(): 
     # return redirect(url_for("Love.html"))
     return render_template("love3.html")
-@love.route('/love4')
+@app.route('/love4')
 def love4(): 
     # return redirect(url_for("Love.html"))
     return render_template("love4.html")
-@love.route('/pic')
+@app.route('/pic')
 def pic():
     return render_template("pic.html")
 
 if __name__ == '__main__':
-    love.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
